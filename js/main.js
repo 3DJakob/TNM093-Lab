961cc0024e2417795bbc908cab0d3bc9dbf78278
Jakob <jakob@folkdatorn.se>
2020-09-29 22:36:08 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+  Computer exercise for TNM093
+  Course responsible: Alexander Bock
+  Created by: Kahin Akram
+  August 2020
+ */
+
+queue()
+  .defer(d3.csv, 'data/socialScience.csv')
+  .await(draw)
+
+var sp, pc, info
+
+function draw (error, data) {
+  if (error) throw error
+
+  //   console.log(data)
+
+  sp = new sp(data)
+  pc = new pc(data)
+  info = new info(data)
+}
